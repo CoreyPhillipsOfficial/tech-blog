948efd10c768f9212b5eddcb5bdae44c9c4b7392
CoreyPhillipsOfficial <coreytphillips@icloud.com>
2023-10-22 16:28:02 -0400

    created server.js and view_routes

---
+++ models/User.js
@@ -0,0 +1,26 @@
+// Import Model and DataTypes from sequelize
+const { Model, DataTypes } = require('sequelize');
+
+// Create a User class and extend the Model class
+class User extends Model { }
+
+// Call User.init and setup a couple columns/fields - email & password as text strings
+User.init({
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true
+        }
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            min: 6
+        }
+    }
+}, {});
+
+// Export the User model
