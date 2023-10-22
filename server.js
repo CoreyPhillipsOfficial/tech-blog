1657fd51aaae182a034b3ec334c1fc00f3416265
CoreyPhillipsOfficial <coreytphillips@icloud.com>
2023-10-22 12:10:03 -0400

    added starter comments

---
+++ server.js
@@ -0,0 +1,19 @@
+// Import express
+const express = require('express');
+
+// Import our view_routes
+
+
+// Create the port and prepare for heroku with the process.env.PORT value
+
+
+// Create the server app
+
+
+// Open the static channel for our browser assets - ie. express.static on the public folder
+
+
+// Load our view routes at the root level '/'
+
+
+// Start the server and log the port that it started on
