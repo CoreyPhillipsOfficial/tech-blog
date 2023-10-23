13cd526194c998796af3bddac75ba5355967fbd3
CoreyPhillipsOfficial <coreytphillips@icloud.com>
2023-10-22 21:38:05 -0400

    created auth.js

--- controllers/user_routes.js
+++ controllers/user_routes.js
@@ -1,20 +1,27 @@
 const router = require('express').Router();
 
 // Import the User model
-const User = require('/models/User.js');
+const User = require('../models/User.js');
 
+// localhost:3333/auth/signup
 // Post request route that retrieves the form data (email, password) and creates a new user in the database, using our User model
 // The route will respond with a data object with a property of message that says 'User added successfully!'
-router.post('/signup', (req, res) => {
-    const data = req.body,
+router.post('/signup', async (req, res) => {
+    // const data = req.body;
+    // User.create(data)
+    //     .then(newUser => {
+    //         res.json({
+    //             message: 'User added successfully!',
+    //             user: newUser
+    //         });
+    //     });
+    try {
+        await User.create(req.body);
 
-        User.create(data)
-            .then(newUser => {
-                res.json({
-                    message: 'User added successfully!',
-                    user: newUser
-                });
-            });
+        res.redirect('/');
+    } catch (error) {
+        res.redirect('/register');
+    }
 });
 
 module.exports = router;
